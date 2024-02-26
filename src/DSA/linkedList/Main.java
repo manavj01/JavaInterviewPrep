d926d2a03b53299260a9017ef477153923080f20
Manav Jain <manavj0120@gmail.com>
2024-02-26 21:18:02 +0530

    Niki training comments added

--- src/DSA/linkedList/Main.java
+++ src/DSA/linkedList/Main.java
@@ -1,10 +1,10 @@
 package DSA.linkedList;
 
-public class Main {
+public class Main { // Main class to perform and create various functions for linkedlist
     public static void main(String[] args) {
-        Node head = new Node(10);
-        Node second = new Node(20);
-        head.next = second;
+        Node head = new Node(10); // created a head node with some data
+        Node second = new Node(20); // created another node with some data
+        head.next = second; // linked the head node with second node and the process continues down below.
         Node third = new Node(30);
         second.next = third;
         Node fourth = new Node(40);
@@ -13,16 +13,16 @@ public class Main {
         fourth.next = fifth;
 
 
-        printList(head);
+        printList(head); // function to print all nodes in a linkedlist
 
     }
 
     public static void printList(Node head){
-        Node temp = head;
+        Node temp = head; // temporary node that points to head
 
-        while (temp != null){
-            System.out.println(temp.data);
-            temp = temp.next;
+        while (temp != null){ // loop that runs from head till null
+            System.out.println(temp.data); // prints the node
+            temp = temp.next; // increment the node from prev to next
         }
 
     }
--- src/DSA/linkedList/Node.java
+++ src/DSA/linkedList/Node.java
@@ -1,18 +1,20 @@
 package DSA.linkedList;
 
-public class Node {
-    int data;
-    Node next = null;
+public class Node { // node class or a blueprint of what a node in a linkedlist should look like.
+    int data; // data attribute
+    Node next = null; // next node attribute that
+                    // points to the next node but initially points to null when created new
 
-    Node(){
-        next = null;
+    Node(){  // default constructor
+
     }
 
-    Node(int data, Node next){
+    Node(int data, Node next){ // parameterised constructor that assigns value of data
+                                // and next node attributes
         this.data = data;
         this.next = next;
     }
-    Node(int data){
+    Node(int data){ // parameterised constructor that assigns node its value
         this.data = data;
     }
 }
